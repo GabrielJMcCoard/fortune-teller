faac1a7790bc15c6a2c07b4bd38f6056c3f89244
unknown <gabrieljmccoard@gmail.com>
2018-09-10 11:35:07 -0400

    initial commit

---
+++ src/FortuneTeller.java
@@ -0,0 +1,86 @@
+import java.util.Scanner;
+
+public class FortuneTeller{
+
+	
+	//private static Object secondString;
+
+	public static void main(String[] args) {
+		
+		System.out.println("What is your first name?");
+		Scanner firstname = new Scanner(System.in);		
+		System.out.println(firstname.nextLine());;
+		
+		System.out.println("What is your last name?");
+		Scanner lastname = new Scanner(System.in);
+		System.out.println(lastname.nextLine());
+		
+		System.out.println("What is your age?");
+		Scanner age = new Scanner(System.in);
+		System.out.println(age.nextLine());
+		
+		System.out.println("What month were you born?");
+		Scanner birthmonth = new Scanner(System.in);
+		System.out.println(birthmonth.nextLine());
+		
+		
+		
+		
+		
+		System.out.println("What is your favorite ROYGBIV color? If you do not know what ROYGBIV is, enter 'help'");
+		Scanner help = new Scanner(System.in);
+		
+		String firststring = help.nextLine();
+		
+		if (firststring.equals("help")) {
+		System.out.println("red, orange, yellow, green, blue, indigo, violet");
+		 
+			
+		
+		
+		
+		//System.out.println(help.nextLine());
+		
+		
+		
+				
+				
+		
+		
+	
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("How many siblings do you have?");
+		Scanner siblings = new Scanner(System.in);
+		System.out.println(siblings.nextLine());
+	
+		
+		//need increment	
+		
+		System.out.println("How many siblings do you have?");
+		String name = in.nextLine();
+		Ssystem.out.print("how many siblings do you have")
+		int numberOfsibling = sibling.nextLine();
+		If (sibling=0){
+		
+				
+
+		
+		
+		} 
+		}	
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
